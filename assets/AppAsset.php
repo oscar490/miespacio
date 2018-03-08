0c6fe91fb999aa0864a3777c58d4bbd81562a411
Óscar Vega Herrera <oscar.vega@iesdonana.org>
2018-03-08 18:07:11 +0100

    Al seleccionar un elemento de menú, se señala el menú de usuario.

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -23,6 +23,7 @@ class AppAsset extends AssetBundle
         'css/site.css',
     ];
     public $js = [
+        'js/iteracion.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
---
+++ web/js/iteracion.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+    seleccionarMenuUsuario();
+
+})
+
+/**
+ * Cuando se selecciona una de las opciones del menú desplegable
+ * de opciones de usuario, el color de fondo del elemento de menú
+ * cambia de color para indicar que nos encontramos en esa posisción.
+ * @return {[type]} [description]
+ */
+function seleccionarMenuUsuario() {
+    let elementoActivo = $('ul.dropdown-menu .active');
+
+    if (elementoActivo.length > 0) {
+        elementoActivo.parent().prev().css('backgroundColor', '#75a4c1');
+    }
+}
