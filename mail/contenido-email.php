40efa695ca22babcbd07dd73054719d612e60725
Óscar Vega <oscarvegaherrera59@gmail.com>
2018-03-09 20:41:33 +0100

    Mejorando estilo de cuerpo de correo.

--- mail/contenido-email.php
+++ mail/contenido-email.php
@@ -1,18 +1,58 @@
 <?php
 
 use yii\helpers\Html;
-use yii\helpers\Url;
-use Yii;
 
-$css = <<<EOT
-    
-    .cabecera {
-        background-color: blue;
-        color: white;
+$css = <<<'EOT'
+
+    .contenedor {
+        border: solid 3px black;
+        width: 500px;
+        background-color: #0266a0;
+        display: flex;
+        flex-direction: column;
+        border-radius: 8px;
+
     }
 
-    .cuerpo {
+    .cabecera {
+        background-color: #0266a0;
+        color: white;
+        display: flex;
+        justify-content: center;
+        border-radius: 8px;
+    }
+
+    .contenido {
         background-color: white;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        border-radius: 8px;
+    }
+
+    .envio {
+        background-color: #48a54e;
+        width: 420px;
+        display: flex;
+        justify-content: center;
+        margin-bottom: 20px;
+        border-radius: 5px;
+    }
+
+    .fila {
+        display: flex;
+        justify-content: center;
+    }
+
+
+    a:hover, a {
+        color: white;
+        text-decoration: none;
+    }
+
+    img {
+        width: 60px;
+        height: 60px;
     }
 EOT;
 
@@ -22,26 +62,53 @@ $this->registerCss($css);
 
 <div class='contenedor'>
     <div class='cabecera'>
+        
         <?=
             Html::tag(
-                'h2',
-                'Confirmar el correo electrónico nuevo
-                 de ' . Yii::$app->name
+                'h3',
+                Html::tag(
+                    'strong',
+                    Yii::$app->name
+                )
             );
 
         ?>
     </div>
-    <div class='cuerpo'>
-        <?=
-            Html::tag(
-                'p',
-                'Ha enviado una solicitud para añadir
-                 el correo electrónico a su cuenta'
-            )
-        ?>
+    <div class='contenido'>
+        <div class='fila'>
+            <?=
+                Html::tag(
+                    'h4',
+                    Html::tag(
+                        'strong',
+                        'Confirmar el correo electrónico nuevo'
+                    )
+                );
+            ?>
+        </div>
+        <div class='fila'>
+            <?=
+                Html::tag(
+                    'p',
+                    'Ha enviado una solicitud para añadir
+                     el correo electrónico a su cuenta'
+                )
+            ?>
+        </div>
 
-        <?=
-            Html::a('Click aqui', ['site/index'])
-        ?>
+        <div class='fila'>
+            <?=
+                Html::a(
+                    Html::tag(
+                        'div',
+                        'Haz click aquí para confirmar esta dirección de
+                         correo electrónico',
+                        ['class' => 'envio']
+                    ),
+                    ['site/index']
+                );
+            ?>
+        </div>
     </div>
+
 </div>
--- views/site/contenido-email.php
+++ views/site/contenido-email.php
@@ -3,7 +3,59 @@
 use yii\helpers\Html;
 use yii\helpers\Url;
 
+$css = <<<EOT
 
+    .contenedor {
+        border: solid 3px black;
+        width: 500px;
+        background-color: #0266a0;
+        display: flex;
+        flex-direction: column;
+        border-radius: 8px;
+
+    }
+
+    .cabecera {
+        background-color: #0266a0;
+        color: white;
+        display: flex;
+        justify-content: center;
+        border-radius: 8px;
+    }
+
+    .contenido {
+        background-color: white;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        border-radius: 8px;
+    }
+
+    .envio {
+        background-color: #48a54e;
+        width: 420px;
+        display: flex;
+        justify-content: center;
+        margin-bottom: 20px;
+        border-radius: 5px;
+    }
+
+    .fila {
+        display: flex;
+        justify-content: center;
+    }
+
+
+    a:hover, a {
+        color: white;
+        text-decoration: none;
+    }
+
+    img {
+        width: 60px;
+        height: 60px;
+    }
+EOT;
 
 $this->registerCss($css);
 
@@ -12,24 +64,56 @@ $this->registerCss($css);
 <div class='contenedor'>
     <div class='cabecera'>
         <?=
+            Html::img(
+                '/images/logotipo.png'
+            );
+        ?>
+        <?=
             Html::tag(
                 'h3',
-                 Yii::$app->name
+                Html::tag(
+                    'strong',
+                    Yii::$app->name
+                )
             );
 
         ?>
     </div>
-    <div>
-        <?=
-            Html::tag(
-                'p',
-                'Ha enviado una solicitud para añadir
-                 el correo electrónico a su cuenta'
-            )
-        ?>
+    <div class='contenido'>
+        <div class='fila'>
+            <?=
+                Html::tag(
+                    'h4',
+                    Html::tag(
+                        'strong',
+                        'Confirmar el correo electrónico nuevo'
+                    )
+                );
+            ?>
+        </div>
+        <div class='fila'>
+            <?=
+                Html::tag(
+                    'p',
+                    'Ha enviado una solicitud para añadir
+                     el correo electrónico a su cuenta'
+                )
+            ?>
+        </div>
 
-        <?=
-            Html::a('Click aqui', ['site/index'])
-        ?>
+        <div class='fila'>
+            <?=
+                Html::a(
+                    Html::tag(
+                        'div',
+                        'Haz click aquí para confirmar esta dirección de
+                         correo electrónico',
+                        ['class'=>'envio']
+                    ),
+                    ['site/index']
+                );
+            ?>
+        </div>
     </div>
+
 </div>
