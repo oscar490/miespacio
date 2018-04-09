1a4150152301efae2a8476f913940a85ad3231b4
Óscar Vega Herrera <oscar.vega@iesdonana.org>
2018-04-09 19:11:51 +0200

    Trabajando con la modificación del color del tablero.

--- views/tableros/view.php
+++ views/tableros/view.php
@@ -17,12 +17,30 @@ $this->params['breadcrumbs'][] = [
 ];
 
 $css = <<<EOT
-    .rojo {
-        background-color: red;
+
+
+    a:link {
+        text-decoration: none;
     }
 EOT;
 
+$js = <<<EOT
+    $('#btn_color').on('click', function() {
+        let ventana = window.open(
+            "html/colores.html",
+            'Seleccion de color',
+            'resizable=0, width=600px, height=500px, top=300px, left=300px'
+        );
+    })
+
+    function cambiarColor(color) {
+        alert(color);
+    }
+EOT;
+
+$this->registerJS($js);
 $this->registerCss($css);
+
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="container">
@@ -34,10 +52,18 @@ $this->params['breadcrumbs'][] = $this->title;
                         Html::encode($model->denominacion)
                     ?>
                 </strong>
+
+                <?=
+                    Html::a(
+                        $model->equipo->denominacion,
+                        ['equipos/view', 'id' => $model->equipo->id],
+                        ['class'=>'btn-sm btn-info']
+                    );
+                ?>
             </h4>
         </div>
         <div class='col-md-3 col-md-offset-9'>
-            <div class='panel panel-default'>
+            <div class='panel panel-primary'>
                 <div class='panel-heading'>
                     <strong>
                         <?= Html::encode('Propiedades') ?>
@@ -63,14 +89,20 @@ $this->params['breadcrumbs'][] = $this->title;
                 </div>
             </div>
 
-            <div class='panel panel-default'>
+            <div class='panel panel-primary'>
                 <div class='panel-heading'>
                     <?= Html::encode('Color') ?>
                 </div>
                 <div class='panel-body'>
-                    <div class='rojo'>
-                        hola
-                    </div>
+                    <?=
+                        Html::button(
+                            'Cambiar',
+                            [
+                                'class'=>'btn btn-success btn-block',
+                                'id'=>'btn_color',
+                            ]
+                        );
+                    ?>
                 </div>
             </div>
         </div>
---
+++ web/js/colores.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.cuadro').on('click', function() {
+        let color = $(this).css('backgroundColor');
+        opener.document.cambiarColor(color);
+    })
+
+
+
+
+
+
+
+
+})
